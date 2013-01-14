85fa27a57d24c83da7dd7fdfb841daa742b3c7e8
boris <boris@13901e78-2507-634d-b62a-01ba92d34a10>
2013-01-14 14:31:11 +0000

    2888 Выложить текущий результат работы на домен в .artektiv.ru http://task.artektiv.ru/dev/view/id/2888

--- index.php
+++ index.php
@@ -6,11 +6,14 @@ $yii=dirname(__FILE__).'/../../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 if ($_SERVER['HTTP_HOST'] == 'twt.local') {
-	$yii= 'yii-1.1.13/yii.php';
+	$yii = 'yii-1.1.13/yii.php';
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
+} else if ($_SERVER['HTTP_HOST'] == 'twt-erp.artektiv.ru') {
+	$yii = 'framework/yii.php';
+} else {
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
 }
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
