041b0a3aa0b86b8ebf4deaa1099fd2daecb36aed
boris <boris@13901e78-2507-634d-b62a-01ba92d34a10>
2013-01-14 15:27:50 +0000

    Паша пришел и говорил как херачить!

--- protected/modules/legal/controllers/DefaultController.php
+++ protected/modules/legal/controllers/DefaultController.php
@@ -4,6 +4,13 @@ class DefaultController extends Controller
 {
 	public function actionIndex()
 	{
-		$this->render('index');
+		$data = array();
+		if ($_POST && !empty($_POST['method'])) {
+			$data = $_POST;
+			$method = $_POST['method'];
+			$ret = call_user_func_array(array(Yii::app()->soap, $method), !empty($_POST['args']) ? json_decode($_POST['args']) : array());
+			CVarDumper::dump($ret, 3, 1);
+		}
+		$this->render('index', array('data' => $data));
 	}
 }
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,11 +3,11 @@
 Yii::app()->clientScript->registerCssFile(CHtml::asset(Yii::app()->basePath.'/../static/css/main.css'));
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=Yii::app()->language?>" lang="<?=Yii::app()->language?>">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+	<meta name="language" content="<?=Yii::app()->language?>" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
@@ -22,33 +22,33 @@ Yii::app()->clientScript->registerCssFile(CHtml::asset(Yii::app()->basePath.'/..
 </div>
 <div class="container">
 	<div class="row">
-	<div class="span3">
-		<?php
-		$items = array(
-			'main'           => array('label' => 'Главная', 'url' => Yii::app()->homeUrl),
-			'legal.entities' => array('label' => 'Юридические лица', 'url' => $this->createUrl('/legal/entities/')),
-			//'legal.Counterparties_groups' => array('label' => 'Группы контрагентов', 'url' => $this->createUrl('/legal/counterparties_groups/')),
-			//'legal.Banks' => array('label' => 'Банки', 'url' => $this->createUrl('/legal/banks/')),
-			'legal.Countries' => array('label' => 'Страны юрисдикции', 'url' => $this->createUrl('/legal/countries/')),
-		);
-		if (isset($items[$this->menu_elem])) {
-			$items[$this->menu_elem]['active'] = true;
-		}
-		$this->widget('bootstrap.widgets.TbMenu', array(
-				'type'      => 'tabs', // '', 'tabs', 'pills' (or 'list')
-			'stacked'   => true, // whether this is a stacked menu
-			'items'     => array_values($items)
-		));
-		unset($items); ?>
-	</div>
-		<div class="span9">
+		<div class="span3">
 			<?php
-			/*$this->widget('bootstrap.widgets.TbBreadcrumbs', array(
-				'links' => $this->breadcrumbs
-			));*/?>
-		<?=$content?>
+			$items = array(
+				'main'           => array('label' => 'Главная', 'url' => Yii::app()->homeUrl),
+				'legal.entities' => array('label' => 'Юридические лица', 'url' => $this->createUrl('/legal/entities/')),
+				//'legal.Counterparties_groups' => array('label' => 'Группы контрагентов', 'url' => $this->createUrl('/legal/counterparties_groups/')),
+				//'legal.Banks' => array('label' => 'Банки', 'url' => $this->createUrl('/legal/banks/')),
+				'legal.Countries' => array('label' => 'Страны юрисдикции', 'url' => $this->createUrl('/legal/countries/')),
+			);
+			if (isset($items[$this->menu_elem])) {
+				$items[$this->menu_elem]['active'] = true;
+			}
+			$this->widget('bootstrap.widgets.TbMenu', array(
+					'type'      => 'tabs', // '', 'tabs', 'pills' (or 'list')
+				'stacked'   => true, // whether this is a stacked menu
+				'items'     => array_values($items)
+			));
+			unset($items); ?>
+		</div>
+		<div class="span9">
+				<?php
+				/*$this->widget('bootstrap.widgets.TbBreadcrumbs', array(
+					'links' => $this->breadcrumbs
+				));*/?>
+			<?=$content?>
+		</div>
 	</div>
-</div>
 </div>
 
 </body>
