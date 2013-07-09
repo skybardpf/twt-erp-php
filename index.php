8cc7f9c836030a6e64794667af4f9baed59069e5
skybardpf <skybardpf@13901e78-2507-634d-b62a-01ba92d34a10>
2013-07-09 07:12:21 +0000

    http://task.artektiv.ru/dev/view/id/6998
    Fix. Путь к WSDL файлу с настройками.

--- index.php
+++ index.php
@@ -1,31 +1,15 @@
 <?php
+$yii = 'yii.php';
 
-// change the following paths if necessary
-
-$yii    = dirname(__FILE__).'/../../yii/framework/yii.php';
-$config = dirname(__FILE__).'/protected/config/main.php';
-
-if ($_SERVER['HTTP_HOST'] == 'twt.local') {
-	$yii = 'yii-1.1.13/yii.php';
-	defined('YII_DEBUG') or define('YII_DEBUG',true);
-} elseif ($_SERVER['HTTP_HOST'] == 'yury.twt-1c.ru') {
-	$yii = dirname(__FILE__).'/../yii/framework/yii.php';
-	defined('YII_DEBUG') or define('YII_DEBUG',true);
-} elseif ($_SERVER['HTTP_HOST'] == 'twt-erp.artektiv.ru') {
-	$yii = 'framework/yii.php';
-} elseif ($_SERVER['HTTP_HOST'] == 'twt-1c') {
-    $yii = dirname(__FILE__).'/../yii/yii.php';
+if ($_SERVER['HTTP_HOST'] == 'twt-1c') {
     defined('YII_DEBUG') or define('YII_DEBUG',true);
-} elseif ($_SERVER['HTTP_HOST'] == 'local.twt-1c.ru') {
-//    $yii = dirname(__FILE__).'/../yii/yii.php';
-    defined('YII_DEBUG') or define('YII_DEBUG',true);
-}else {
-	defined('YII_DEBUG') or define('YII_DEBUG',true);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+    $config = dirname(__FILE__).'/protected/config/dev.php';
+} else {
+    defined('YII_DEBUG') or define('YII_DEBUG',false);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
+    $config = dirname(__FILE__).'/protected/config/production.php';
 }
-
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
-
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
