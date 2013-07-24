9a769cb264f1c91f5c95ed9d56da5d6d809c70fd
leadert <gitolite@local.git>
2013-07-24 14:00:17 +0400

    debug=on

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ if ($_SERVER['HTTP_HOST'] == 'twt-erp') {
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
     $config = dirname(__FILE__).'/../protected/config/dev.php';
 
-} elseif ($_SERVER['HTTP_HOST'] == 'twt-erp.artektiv.ru') {
-    defined('YII_DEBUG') or define('YII_DEBUG',true);
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-    $config = dirname(__FILE__).'/../protected/config/production.php';
+//} elseif ($_SERVER['HTTP_HOST'] == 'twt-erp.artektiv.ru') {
+//    defined('YII_DEBUG') or define('YII_DEBUG',true);
+//    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+//    $config = dirname(__FILE__).'/../protected/config/production.php';
 } else {
 //    defined('YII_DEBUG') or define('YII_DEBUG',false);
 //    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
