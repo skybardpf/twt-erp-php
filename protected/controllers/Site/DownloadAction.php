8e77c86b9724d2fc9eb7daaa61d492da3fd5bb92
Skibardin Andrey <skybardpf@artektiv.ru>
2013-08-23 12:46:20 +0400

    Загрузка заявок по калькулятору

--- protected/controllers/Site/DownloadAction.php
+++ protected/controllers/Site/DownloadAction.php
@@ -6,11 +6,11 @@ class DownloadAction extends CAction
 {
     public function run($path)
     {
-//        $encoded = strtr(base64_encode(addslashes(gzcompress(serialize($string),9))), '+/=', '-_,');
+//        $path = strtr(base64_encode(addslashes(gzcompress(serialize('Прототипы.Библиотека шаблонов.pdf'),9))), '+/=', '-_,');
         $path = unserialize(gzuncompress(stripslashes(base64_decode(strtr($path, '-_,', '+/=')))));
         $path = Yii::getPathOfAlias('filestorage').DIRECTORY_SEPARATOR.$path;
         if (!file_exists($path)){
-            echo 'Файл не существует';
+            echo 'NotFound';
             die;
         }
 
