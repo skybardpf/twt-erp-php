9a7ad83ef0afdebef25f60895f325c68dfb0da75
Skibardin Andrey <webprofi1983@gmail.com>
2013-09-10 18:06:13 +0400

    Корзина акционирования. Часть 1

---
+++ protected/views/cart_corporatization/tab_cart.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Просмотр корзины акционирования.
+ *
+ * @author Skibardin A.A. <webprofi1983@gmail.com>
+ *
+ * @var Cart_corporatizationController $this
+ * @var string $cur_tab
+ */
+?>
