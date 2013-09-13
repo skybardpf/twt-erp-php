8c9552f0d1d8634c5f53a31a97d1965591114b01
Skibardin Andrey <webprofi1983@gmail.com>
2013-09-13 15:46:30 +0400

    Disabled поля при редактировании стандартного вида договора

--- protected/controllers/ContractType/UpdateAction.php
+++ protected/controllers/ContractType/UpdateAction.php
@@ -18,9 +18,6 @@ class UpdateAction extends CAction
         $controller->pageTitle .= ' | Редактирование вида договора';
 
         $model = ContractType::model()->findByPk($id, $controller->getForceCached());
-        if ($model->is_standart)
-            throw new CHttpException(403, 'Нельзя редактировать стандартный вид договора');
-
         if(isset($_POST['ajax']) && $_POST['ajax']==='form-type-contract') {
             echo CActiveForm::validate($model);
             Yii::app()->end();
@@ -28,6 +25,9 @@ class UpdateAction extends CAction
 
         $data = Yii::app()->request->getPost(get_class($model));
         if ($data) {
+            if ($model->is_standart)
+                throw new CHttpException(403, 'Нельзя редактировать стандартный вид договора');
+
             $model->setAttributes($data);
             if ($model->validate()) {
                 try {
